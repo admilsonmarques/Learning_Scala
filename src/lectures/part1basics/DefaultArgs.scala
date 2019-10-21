271be8f91b7059fa2bcf650141c0c48b0b340560
Admilson Marques <admilson.marques@nubank.com.br>
2019-10-21 14:42:43 -0300

    8th Lesson

---
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -0,0 +1,24 @@
+package lectures.part1basics
+
+object DefaultArgs extends App{
+
+  def trFactorial(n: Int, acc: Int = 1): Int =
+    if (n<=1) acc
+    else trFactorial(n-1,n*acc)
+
+  val fact10 = trFactorial(10)
+
+
+  def savePict(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit = println("saving picture")
+  savePict(width = 800)
+/*
+1. Pass in every leading argument
+2. name the arguments
+savePict(width = 800)
+
+ */
+
+  savePict(height=600, width = 800, format = "bitmap")
+  
+
+}
